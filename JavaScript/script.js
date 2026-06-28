dae311fe15d295382f78c95601c68a2a2756345a
ashin <ashinsaparami2002@gmail.com>
2026-06-28 19:55:06 +0530

    functional requirement table update in camera rental management system web page

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -80,3 +80,36 @@ const observer = new IntersectionObserver(entries => {
 counters.forEach(counter => {
     observer.observe(counter);
 });
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+const menuIcon = menuToggle.querySelector("i");
+
+menuToggle.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+    if(navLinks.classList.contains("active")){
+        menuIcon.classList.remove("fa-bars");
+        menuIcon.classList.add("fa-times");
+    }else{
+        menuIcon.classList.remove("fa-times");
+        menuIcon.classList.add("fa-bars");
+    }
+
+});
+
+const navItems = document.querySelectorAll(".nav-links a");
+
+navItems.forEach(item => {
+
+    item.addEventListener("click", () => {
+
+        navLinks.classList.remove("active");
+
+        menuIcon.classList.remove("fa-times");
+        menuIcon.classList.add("fa-bars");
+
+    });
+
+});
